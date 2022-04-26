d9f6207aa525113e4ae64c6375678f82ffb587d0
William Kevin Broxton <90276407+wkbroxton@users.noreply.github.com>
2022-04-26 15:15:35 -0700

    server addition to index

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
-// add your code here
+// Middelware for loggin in Morgan
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.status(200).send("ok");
+});
+
+app.get("/api/TodoItems", (req, res) => {});
+
+app.post("api/TodoItems", (req, res) => {});
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
-const server = require('./app');
+const server = require("./app");
 
-// write your code here
+server.listen(3000, () => {
+  console.log(`server is listening on 3000`);
+});

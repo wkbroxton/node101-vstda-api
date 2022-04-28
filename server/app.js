2927f097c5721b2347492e36e1255d26d7f23e60
William Kevin Broxton <90276407+wkbroxton@users.noreply.github.com>
2022-04-28 13:25:43 -0700

    data moved to app.js and first get api

--- server/app.js
+++ server/app.js
@@ -1,18 +1,50 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+const data = [
+  {
+    todoItemId: 0,
+    name: "an item",
+    priority: 3,
+    completed: false,
+  },
+  {
+    todoItemId: 1,
+    name: "another item",
+    priority: 2,
+    completed: false,
+  },
+  {
+    todoItemId: 2,
+    name: "a done item",
+    priority: 1,
+    completed: true,
+  },
+];
+
 // Middelware for logging in Morgan
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 
+// Routes
 app.get("/", (req, res) => {
-  res.status(200).send("ok");
+  res.status(200).send({ status: "ok" });
 });
 
-app.get("/api/TodoItems", (req, res) => {});
+app.get("/api/TodoItems", (req, res) => {
+  res.status(200).json(data);
+});
 
-app.post("api/TodoItems/{id}", (req, res) => {});
+app.get("/api/TodoItems/:id", (req, res) => {
+  res
+    .status(200)
+    .json(data.filter((todoItem) => todoItem.todoItemId == req.params.id)[0]);
+});
+
+app.post("api/TodoItems/:id", (req, res) => {});
 
 app.post("/api/TodoItems/:number", (req, res) => {});
 
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const server = require("./app");
 
-server.listen(3000, () => {
-  console.log(`server is listening on 3000`);
+server.listen(8484, () => {
+  console.log(`server is listening on 8484`);
 });
